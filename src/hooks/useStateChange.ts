d6e2640203e9e0cfdb22bb9adab7c031523229ae
Dana Basken <dbasken@gmail.com>
2023-02-11 18:07:01 -0500

    added useStateChange, updated release to commit coverage

---
+++ src/hooks/useStateChange.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright ©2023 Dana Basken
+ */
+
+import {useEffect, useState} from "react";
+import {EventBus} from "../eventbus/EventBus";
+import {Logger} from "../logger/Logger";
+import {State} from "../state/State";
+
+const logger = Logger.logger;
+
+export default function useStateChange<T>(key: string): any {
+
+  const [value, setValue] = useState<T>();
+
+  useEffect(() => {
+    const registration = State.register(key, (event: any) => setValue(event.value));
+    return () => {
+      EventBus.unregister(registration);
+    };
+  }, [key]);
+
+  return value;
+
+}
